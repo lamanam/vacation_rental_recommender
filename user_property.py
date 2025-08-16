216145450702f832a81032d380af3aad679a1f57
Harrison Lou <144875450+nanlou888@users.noreply.github.com>
2025-08-15 22:14:22 -0400

    new user files

--- user_property.py
+++ user_property.py
@@ -1,8 +1,9 @@
 # property & user defs
 
 class Property:
-    def __init__(self, property_id, location, type_, price_per_night, features, tags):
+    def __init__(self, property_id, name, location, type_, price_per_night, features, tags):
         self.property_id = property_id
+        self.name = name
         self.location = location
         self.type = type_
         self.price_per_night = price_per_night
@@ -12,6 +13,7 @@ class Property:
     def to_dict(self):
         return {
             "property_id": self.property_id,
+            "name":self.name,
             "location": self.location,
             "type": self.type,
             "price_per_night": self.price_per_night,
@@ -23,6 +25,7 @@ class Property:
     def from_dict(cls, d):
         return cls(
             d["property_id"],
+            d["name"],
             d["location"],
             d["type"],
             d["price_per_night"],
@@ -32,12 +35,13 @@ class Property:
 
 
 class User:
-    def __init__(self, user_id, name, group_size, preferred_environment, budget):
+    def __init__(self, user_id, name, group_size, preferred_environment, budget_range, travel_dates):
         self.user_id = user_id
         self.name = name
         self.group_size = group_size
         self.preferred_environment = preferred_environment
-        self.budget = budget
+        self.budget_range = budget_range
+        self.travel_dates = travel_dates
 
     def to_dict(self):
         return {
@@ -45,7 +49,8 @@ class User:
             "name": self.name,
             "group_size": self.group_size,
             "preferred_environment": self.preferred_environment,
-            "budget": self.budget
+            "budget_range": self.budget_range,
+            "travel_dates": self.travel_dates
         }
 
     @classmethod
@@ -55,5 +60,6 @@ class User:
             d["name"],
             d["group_size"],
             d["preferred_environment"],
-            d["budget"]
+            d["budget_range"],
+            d["travel_dates"]
         )
